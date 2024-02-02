c836bc7f61593455276108c3baafa7c16840f80e
Av3z <willyanf17@hotmail.com>
2024-02-02 01:09:19 -0300

    chore: started home page in app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:todo_dio/src/pages/home_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+    );
   }
 }
