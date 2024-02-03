172283f79ac93d853b8c201f5bb1acc15e309ffb
Av3z <willyanf17@hotmail.com>
2024-02-03 11:23:49 -0300

    feat: add inputs for get username

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:todo_dio/src/components/custom_drawer.dart';
+import 'package:todo_dio/src/pages/chat_page.dart';
 
+// ignore: must_be_immutable
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  var nickController = TextEditingController();
+  HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +16,47 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         title: const Text('Home'),
       ),
-      body: Container(),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Digite seu nickname',
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: const Color.fromARGB(31, 85, 84, 84),
+                hintText: '@username',
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(6)),
+              ),
+              controller: nickController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton.icon(
+                label: const Text('Acessar chat'),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) =>
+                              ChatPage(nickname: nickController.text)));
+                },
+                icon: const Icon(Icons.check)),
+          ],
+        ),
+      ),
     ));
   }
 }
