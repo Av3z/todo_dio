fe205c04f92b232a38419a04e44d49f0f142f9f0
Av3z <willyanf17@hotmail.com>
2024-02-03 11:25:47 -0300

    feat: add user and dates to task model

--- lib/src/models/tarefa_model.dart
+++ lib/src/models/tarefa_model.dart
@@ -1,18 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class TarefaModel {
   String descricao = "";
   bool concluido = false;
+  DateTime createdAt = DateTime.now();
+  DateTime updatedAt = DateTime.now();
+  String userId = "";
 
-  TarefaModel({required this.descricao, required, required this.concluido});
+  TarefaModel(
+      {required this.descricao, required this.concluido, required this.userId});
 
   TarefaModel.fromJson(Map<String, dynamic> json) {
     descricao = json['descricao'];
     concluido = json['concluido'];
+    createdAt = (json['createdAt'] as Timestamp).toDate();
+    updatedAt = (json['updatedAt'] as Timestamp).toDate();
+    userId = json['userId'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = {
       'descricao': descricao,
-      'concluido': concluido
+      'concluido': concluido,
+      'createdAt': Timestamp.fromDate(createdAt),
+      'updatedAt': Timestamp.fromDate(updatedAt),
+      'userId': userId
     };
 
     return data;
